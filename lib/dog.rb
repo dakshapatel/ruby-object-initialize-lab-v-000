d9ca568eeb935312f24d769742d2440cc8e10e0f
Daksha Patel <dakshapatel100@gmail.com>
2018-05-07 19:14:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,14 +4,15 @@ class Dog
     @breed = dogs_breed
   end
 
+  def name
+    @name
+  end
+
   def name=(dogs_name, dogs_breed = "Mutt")
     @name = dogs_name
     @breed = dogs_breed
   end
 
-  def name
-    @name
-  end
 
   def breed
     @breed
